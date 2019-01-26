a479ea58549e6985726b562e1a39eac2d3fbb042
hibes <hibes@0ti.me>
2019-01-26 12:51:39 -0800

    Simplify, document, abstract download.js

--- bin/download.js
+++ bin/download.js
@@ -10,6 +10,40 @@ const fname = 'assets.tar.gz';
 const etagFile = `${fname}.etag`;
 const uri = 'https://phaser.0ti.me/tgz';
 
+const status200Handler = response =>
+  Promise.resolve(response)
+    .tap(saveEtag)
+    .then(writeTarGz)
+    .tap(file =>
+      tar.x({
+        file,
+      }),
+    )
+    .then(file => ({
+      file,
+      response,
+    }));
+
+const status304Handler = response =>
+  Promise.resolve(response)
+    .tap(ensure304OrReject)
+    .then(ensureFileLengthMatches)
+    .then(ensureShaMatchesEtag)
+    .then(() => ({response}));
+
+const otherStatusHandler = response => Promise.reject(response);
+
+const responseHandlerMap = {
+  200: status200Handler,
+  304: status304Handler,
+  default: otherStatusHandler,
+};
+
+const responseHandler = response =>
+  (responseHandlerMap[response.statusCode] || responseHandler.default)(
+    response,
+  );
+
 const writeTarGz = response => {
   if (response.statusCode === 304) return fname;
 
@@ -23,7 +57,7 @@ const saveEtag = response => {
   return fse.writeFile(etagFile, etag).then(() => response);
 };
 
-const checkFileLength = response => {
+const ensureFileLengthMatches = response => {
   if (response.statusCode === 304) return response;
 
   const {body, headers} = response;
@@ -38,7 +72,7 @@ const checkFileLength = response => {
   return response;
 };
 
-const checkShaAgainstEtag = response => {
+const ensureShaMatchesEtag = response => {
   if (response.statusCode === 304) return response;
 
   const {body, headers} = response;
@@ -61,34 +95,38 @@ const ensure304OrReject = response =>
     ? Promise.resolve(response)
     : Promise.reject(response);
 
-const main = () =>
-  Promise.resolve(etagFile)
-    .then(fse.readFile)
+const constructRequestOptions = etag => ({
+  encoding: null,
+  headers: {
+    'If-None-Match': etag,
+  },
+  qs: {
+    paths: ['assets', 'test2'].join(','),
+  },
+  resolveWithFullResponse: true,
+  uri,
+});
+
+const loadSavedEtag = etagFilename =>
+  fse
+    .readFile(etagFilename)
     .then(etagBuffer => etagBuffer.toString())
-    .catch(() => undefined) // default to an etag of undefined
-    .then(etag => ({
-      encoding: null,
-      headers: {
-        'If-None-Match': etag,
-      },
-      qs: {
-        paths: ['assets', 'test2'].join(','),
-      },
-      resolveWithFullResponse: true,
-      uri,
-    }))
+    .catch(() => undefined); // default to an etag of undefined
+
+const main = () =>
+  loadSavedEtag()
+    // Build the request options
+    .then(constructRequestOptions)
+    // Make the request
     .then(rp)
-    .catch(ensure304OrReject)
-    .then(checkFileLength)
-    .then(checkShaAgainstEtag)
-    .catch(err => Promise.reject(err.body || err))
-    .tap(saveEtag)
-    .then(writeTarGz)
-    .then(file =>
-      tar.x({
-        file,
-      }),
-    )
+    // Always resolve, let responseHandler decide next
+    .catch(response => response)
+    .then(responseHandler)
+    // If rejection has a body, reject with body, otherwise reject with rejection
+    .catch(rejection => Promise.reject(rejection.body || rejection))
+    // If no error, exit with a zero error code
+    .then(() => process.exit(0))
+    // Log and exit with a non-zero error code
     .catch(err => {
       console.error(err);
 
