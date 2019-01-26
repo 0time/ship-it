2011f0a47d67679213fdf0423e6ea0d3e698e22c
hibes <hibes@0ti.me>
2019-01-26 12:16:22 -0800

    Fix download.js to use new path, new checksum validation

--- bin/download.js
+++ bin/download.js
@@ -2,27 +2,64 @@
 
 const fse = require('fs-extra');
 const rp = require('request-promise');
+const sha256 = require('sha256');
 const tar = require('tar');
 const Promise = require('bluebird');
 
 const fname = 'assets.tar.gz';
 const etagFile = `${fname}.etag`;
-const uri = 'https://phaser.0ti.me';
+const uri = 'https://phaser.0ti.me/tgz';
 
-const saveEtag = ({headers: {etag}}) => fse.writeFile(etagFile, etag);
+const writeTarGz = response => {
+  if (response.statusCode === 304) return fname;
 
-const check304 = response => {
-  console.error(response.statusCode);
-  console.error(response.headers);
+  return fse.writeFile(fname, Buffer.from(response.body)).then(() => fname);
+};
+const saveEtag = response => {
+  if (response.statusCode === 304) return response;
 
-  if (response.statusCode === 304) {
-    console.error('304 not modified');
+  const etag = response.headers.etag;
 
-    process.exit(0);
+  return fse.writeFile(etagFile, etag).then(() => response);
+};
+
+const checkFileLength = response => {
+  if (response.statusCode === 304) return response;
+
+  const {body, headers} = response;
+  const fileLength = headers['x-file-length'];
+
+  if (`${body.length}` !== fileLength) {
+    throw new Error(
+      `body length (${body.length}) did not match file length (${fileLength})`,
+    );
   }
 
-  return Promise.reject(response);
+  return response;
 };
+
+const checkShaAgainstEtag = response => {
+  if (response.statusCode === 304) return response;
+
+  const {body, headers} = response;
+
+  const sha = sha256(body);
+
+  console.error(`sha ? etag | ${sha} ? ${headers.etag}`);
+
+  if (sha !== headers.etag) {
+    const err = new Error(`sha !== etag | ${sha} !== ${headers.etag}`);
+
+    throw err;
+  }
+
+  return response;
+};
+
+const ensure304OrReject = response =>
+  response.statusCode === 304
+    ? Promise.resolve(response)
+    : Promise.reject(response);
 
 const main = () =>
   Promise.resolve(etagFile)
@@ -30,19 +67,28 @@ const main = () =>
     .then(etagBuffer => etagBuffer.toString())
     .catch(() => undefined) // default to an etag of undefined
     .then(etag => ({
+      encoding: null,
       headers: {
         'If-None-Match': etag,
+      },
+      qs: {
+        paths: ['assets', 'test2'].join(','),
       },
       resolveWithFullResponse: true,
       uri,
     }))
-    .tap(console.error)
     .then(rp)
+    .catch(ensure304OrReject)
+    .then(checkFileLength)
+    .then(checkShaAgainstEtag)
     .catch(err => Promise.reject(err.body || err))
-    .catch(check304)
     .tap(saveEtag)
-    .then(({body}) => fse.writeFile(fname, body))
-    .then(() => tar.x(fname))
+    .then(writeTarGz)
+    .then(file =>
+      tar.x({
+        file,
+      }),
+    )
     .catch(err => {
       console.error(err);
 
